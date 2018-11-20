47e9b571850b158349a79cde302341908c2eb0ba
Chimaek <pipiru100@gmail.com>
2018-11-20 21:59:36 +0900

    적과 방해물 수정
    y축 해결

--- flying_Ariplane.py
+++ flying_Ariplane.py
@@ -2,31 +2,31 @@ import pygame
 import random
 
 # 배경색과 창크기 설정
-BGcolor_white=(255,255,255)
-width=500
-height=700
-BG_height= -700
+BGcolor = (255,255,255)
+width = 600
+height = 840
+BG_height = -840
 
-#게임에 집어넣을 객체를 만드는 함수
+# 게임에 집어넣을 객체를 만드는 함수
 def into_Game(obj,x,y):
     global gamepad
     gamepad.blit(obj,(x,y))
 
 def runGame():
-    global gamepad,clock,unit,background,background1
+    global gamepad,clock,unit,BG,BG1
     global bat,fires
-    x=width*0.05
-    y=height*0.8
-    changeX=0
-    backgroundX=0
-    backgroundX2=BG_height
-
+    x = width*0.05
+    y = height*0.85
+    changeX = 0
+    BG_X = 0
+    BG_X2 = BG_height
+    # 적, 방해물 위치 지정
     bat_x = random.randrange(0,width)
-    bat_y = height
-    fire_x=random.randrange(0,width)
-    fire_y=height
+    bat_y = -67
+    fire_x = random.randrange(0,width)
+    fire_y = -140
     random.shuffle(fires)
-    fire=fires[0]
+    fire = fires[0]
 
     #플래그 설정
     crashe=False
@@ -35,45 +35,48 @@ def runGame():
         # event.get()은 게임에서 발생하는 이벤트 반환하는 함수입니다.
         for event in pygame.event.get():
             # 마우스로 창을 닫을경우 플래그를 설정하여 while문을 빠져나옵니다.
-            if event.type==pygame.QUIT:
+            if event.type == pygame.QUIT:
                 crashe=True
             # 키입력시 x좌표를 업데이트함
-            if(event.type==pygame.KEYDOWN):
-                if(event.key==pygame.K_LEFT):
+            if(event.type == pygame.KEYDOWN):
+                if(event.key == pygame.K_LEFT):
                     changeX = -5
-                elif(event.key==pygame.K_RIGHT):
+                elif(event.key == pygame.K_RIGHT):
                     changeX = 5
-            if(event.type==pygame.KEYUP):
-                if(event.key==pygame.K_LEFT or event.key==pygame.K_RIGHT):
+            if(event.type == pygame.KEYUP):
+                if(event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT):
                     changeX = 0
         x += changeX
-
+        # 비행기가 배경화면을 넘어가지 않게 하는 if문
+        if(x<0 or x>width-67):
+            x -= changeX
         # 게임판을 흰색으로 채우기
-        gamepad.fill(BGcolor_white)
-        backgroundX += 2
-        backgroundX2 += 2
-        bat_y-=7
-        if bat_y<=0:
+        gamepad.fill(BGcolor)
+        BG_X += 2
+        BG_X2 += 2
+        # 적이 화면을 넘어가면 재생성하는 if문
+        bat_y += 7
+        if bat_y>=height:
             bat_x = random.randrange(0,width)
-            bat_y = height
-        if fire==None:
-            fire_y -= 30
+            bat_y = -67
+        # 방해물이 없다면 방해물이 나오는 속도를 조절하는 함수
+        if fire == None:
+            fire_y += 30
         else:
-            fire_y -= 15
-
-        if fire_y <=0:
+            fire_y += 15
+        if fire_y >=height:
             fire_x = random.randrange(0,width)
-            fire_y = height
+            fire_y = -140
             random.shuffle(fires)
             fire=fires[0]
 
-        if backgroundX == -BG_height:
-            backgroundX = BG_height
-        if backgroundX2 == -BG_height:
-            backgroundX2 = BG_height
+        if BG_X == -BG_height:
+            BG_X = BG_height
+        if BG_X2 == -BG_height:
+            BG_X2 = BG_height
         # 배경화면 호출함.
-        into_Game(background,0,backgroundX)
-        into_Game(background1,0,backgroundX2)
+        into_Game(BG,0,BG_X)
+        into_Game(BG1,0,BG_X2)
         # 적과 방해물 호출
         into_Game(bat,bat_x,bat_y)
         if fire != None:
@@ -92,7 +95,7 @@ def runGame():
 #게임을 초기화 하고 시작하는 함수입니다.
 def initGame():
     #전역 변수로 설정합니다.
-    global gamepad,clock,unit,background,background1
+    global gamepad,clock,unit,BG,BG1
     global bat,fires
     fires=[]
     #파이게임 라이브러리를 초기화함 꼭 호출해줘야 합니다.
@@ -104,11 +107,10 @@ def initGame():
     #비행기 이미지 로딩
     unit=pygame.image.load('images/aircraft.png')
     # 배경화면 로딩
-    background = pygame.image.load('images/background.png')
-    background1 = background.copy()
-    #적 이미지 추가
+    BG = pygame.image.load('images/background.png')
+    BG1 = BG.copy()
+    # 적 이미지 및 방해물 추가
     bat=pygame.image.load('images/bat.png')
-    # 방해물 추가
     fires.append(pygame.image.load('images/fireball.png'))
     fires.append(pygame.image.load('images/fireball2.png'))
     #불덩어리 2개와 None객체 5개 넣을 리스트
